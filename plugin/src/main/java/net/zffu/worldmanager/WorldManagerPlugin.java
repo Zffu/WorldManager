938cfb263df585736f73472954ee130be97ae06c
Zffu <radi0o0@outlook.com>
2024-11-15 22:59:29 +0100

    feat: added default config

--- plugin/src/main/java/net/zffu/worldmanager/WorldManagerPlugin.java
+++ plugin/src/main/java/net/zffu/worldmanager/WorldManagerPlugin.java
@@ -4,9 +4,12 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class WorldManagerPlugin extends JavaPlugin {
 
+    public static WorldManagerPlugin INSTANCE;
+
     @Override
     public void onEnable() {
-
+        INSTANCE = this;
+        saveDefaultConfig();
     }
 
     @Override
