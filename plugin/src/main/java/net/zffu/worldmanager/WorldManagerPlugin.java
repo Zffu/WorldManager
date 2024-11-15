81657c05dbae31ad51e5c7c611ec1db79e06c73f
Zffu <radi0o0@outlook.com>
2024-11-15 22:47:26 +0100

    feat: added default class

---
+++ plugin/src/main/java/net/zffu/worldmanager/WorldManagerPlugin.java
@@ -0,0 +1,16 @@
+package net.zffu.worldmanager;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class WorldManagerPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
